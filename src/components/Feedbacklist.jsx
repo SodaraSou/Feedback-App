8ba08ccb5c62555665bbc1b500d073eb576f7c2f
Sodara Sou <sodarasou168@gmail.com>
2023-04-29 22:12:12 +0700

    Add Mock Backend

--- src/components/Feedbacklist.jsx
+++ src/components/Feedbacklist.jsx
@@ -1,20 +1,37 @@
+import { motion, AnimatePresence } from "framer-motion";
 import Feedbackitem from "./Feedbackitem";
 import { useContext } from "react";
 import FeedbackContext from "./context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function Feedbacklist() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.lenght === 0) {
+  if (!isLoading && (!feedback || feedback.lenght === 0)) {
     return <p>No Feedback</p>;
   }
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        <Feedbackitem key={item.id} item={item} />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            layout
+          >
+            <Feedbackitem key={item.id} item={item} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
+  // return (
+
+  // );
 }
 
 export default Feedbacklist;
